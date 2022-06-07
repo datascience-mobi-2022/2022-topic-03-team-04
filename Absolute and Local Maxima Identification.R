87fb90786f6f2abff81a92a60e1a1e2ebd871cc7
richardlangi <dx257@stud.uni-heidelberg.de>
2022-06-07 08:16:46 +0200

    Added Threshold for Detecting Local Maxima
    Also deletes the redundant arguments that makes non maxima to 0

--- Absolute and Local Maxima Identification.R
+++ Absolute and Local Maxima Identification.R
@@ -1,10 +1,12 @@
-# Global and Maxima Detection (need to add threshold in if function)
+```{r}
+# Global and Maxima Detection
 maximafunction = function(input){
+  threshold = 0.5
   output = 0
   for (i in 1:(length(input))){
     # 1st Fraction
     if (i == 1){
-      if(input[i]<input[i+1]){
+      if(input[i]<input[i+1] && input[i] > threshold*(max(input))){
         output[i] = input[i]}
       else{
         output[i] = 0}
@@ -12,7 +14,7 @@ maximafunction = function(input){
     
     # 2nd-24th Fraction
     if (i>1 && i<length(input)){
-      if(input[i]<input[i+1] && input[i]>input[i-1]){
+      if(input[i]<input[i+1] && input[i]>input[i-1] && input[i] > threshold*(max(input))){
         output[i] = input[i]}
       else{
         output[i] = 0}
@@ -20,30 +22,22 @@ maximafunction = function(input){
     
     # 25th Fraction
     if (i==length(input)){
-      if(input[i]>input[i-1]){
+      if(input[i]>input[i-1] && input[i] > threshold*(max(input))){
         output[i] = input[i]
       }
       else{
         output[i] = 0}
     }
   }
-  
-  # Only the first 2 highest values are taken. Others = 0
-  for (i in 1:length(output)){
-    if(output[i]==max(output) || output[i]== sort(output, decreasing = TRUE)[2]){
-      output[i] = output [i]}
-    else{
-      output[i] = 0}
-  }
-  return(output)
+  return(output)}
+
+# A Vector for Fraction's Names
+fractionnames = 0
+for (i in 1:25) {
+  fractionnames[i] = paste("Fraction",i,sep="_")
 }
 
-  # A Vector for Fraction's Names
-  fractionnames = 0
-  for (i in 1:25) {
-    fractionnames[i] = paste("Fraction",i,sep="_")
-  }
-
 # Results
-allmaxima_Ctrl = data.frame(apply(tCtrl_Norm, 2, maximafunction), row.names = fractionnames)
-allmaxima_RNase = data.frame(apply(tRNase_Norm, 2, maximafunction), row.names = fractionnames )
+allmaxima_Ctrl = data.frame(apply(t(Ctrl_Norm), 2, maximafunction), row.names = fractionnames)
+allmaxima_RNase = data.frame(apply(t(RNase_Norm), 2, maximafunction), row.names = fractionnames )
+```
